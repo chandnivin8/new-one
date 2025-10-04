fb19c8225ebaef6b00eb83d0f62110756448e221
chandnivin8 <chandnivin8@gmail.com>
2025-10-04 11:28:55 +0530

    commiting the stage

---
+++ gitpro/src/gitpro/gittest.java
@@ -0,0 +1,13 @@
+package gitpro;
+
+public class gittest {
+
+	public gittest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+    System.out.println( "Hello World!" );
+}}
